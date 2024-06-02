4c9b31e816caefc75ba760acf3cf44e0362bc5fe
Aastha Singh <aasthasinghclg4@gmail.com>
2024-06-02 23:33:01 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+// import "./index.css";
+
+
+
+const time = new Date().getHours();
+// const time = 21
+let pr="";
+
+let sty ={
+  color : ' black',
+  fontWeight : 'bold'
+}
+if(time>=1 && time < 11){
+pr="Good Morning";
+sty.color = 'blue';
+}
+else if( time >=11 && time < 19){
+  pr="Good AfterNoon";
+sty.color = 'yellow';
+
+}
+else{
+  pr="Good Night";
+sty.color = 'green';
+
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+    <h1>hello everyone  , <p style={sty}>{pr} </p> </h1>
+  
+  <p>CURRENT TIME IS {new Date().toLocaleTimeString()}</p>
+   </>
   );
 }
 
